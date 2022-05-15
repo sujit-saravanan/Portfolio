079d8a48eb734443674cf7db8da6c379fdbd6e3b
Sujit Saravanan <sms8817@psu.edu>
2022-05-15 02:27:08 -0400

    added basic window and taskbar features

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function toggleActive(ele) {
-    console.log('active = ' + ele.id);
-};
+let activeWindows = [];
 
-function doubleClick(ele) {
-    console.log("Double-click detected");
-};
+
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
 
 if (document.addEventListener) {
     document.addEventListener('contextmenu', function(e) {
-      console.log(document.activeElement.id);
       e.preventDefault();
     }, false);
   } else {
@@ -18,14 +16,44 @@ if (document.addEventListener) {
     });
 }
 
+function showWindow(id) {
+  const currentWindow = document.getElementById(id+"Window");
+  currentWindow.style.display = "block";
+  currentWindow.style.left = (window.innerWidth/2)-(currentWindow.offsetWidth/2) + "px";
+  currentWindow.style.top = (window.innerHeight/2)-(currentWindow.offsetHeight/2) + "px";
+  activeWindows.push(id+"Window");
+  currentWindow.style.zIndex=(activeWindows.length);
+  document.activeElement.blur();
+  addElementToTaskbar(document.getElementById(id+"Name").innerText);
+}
+
+function addElementToTaskbar(name){
+  let taskbarButton = document.createElement('button');
+  taskbarButton.textContent = name;
+  taskbarButton.style.marginLeft = ".1rem";
+  taskbarButton.style.marginRight = ".1rem";
+  taskbarButton.id = "taskbarbutton";
+  document.getElementById("taskbar").insertBefore(taskbarButton, document.getElementById("clock"));
+}
 
 
-
+dragElement(document.getElementById("SkillsWindow"));
+dragElement(document.getElementById("ProjectsWindow"));
+dragElement(document.getElementById("ContactMeWindow"));
 
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  document.getElementById(elmnt.id + "titlebar").onmousedown = dragMouseDown;
+  document.getElementById(elmnt.id + "Titlebar").onmousedown = dragMouseDown;
+  elmnt.onmousedown = updateZIndex;
+  
+  function updateZIndex(e) {
+    activeWindows = activeWindows.filter(item => item !== elmnt.id);
+    activeWindows.unshift(elmnt.id);
+
+    activeWindows.forEach((windowID, index) => document.getElementById(windowID).style.zIndex=(activeWindows.length-index));
+    document.onmouseup = closeDragElement;
+  }
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -47,8 +75,10 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    if ((elmnt.offsetLeft - pos1 > 0) && ((elmnt.offsetLeft - pos1 + elmnt.offsetWidth) < window.innerWidth) && (elmnt.offsetTop - pos2 > 0) && ((elmnt.offsetTop - pos2 + elmnt.offsetHeight) < window.innerHeight)) {
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
   }
 
   function closeDragElement() {
